88c32e17f1f0dbdd72f6c99f14195ba6bac7ea40
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-02-10 16:34:01 -0800

    Add speed functionality
    Co-authored-by: Andy Plymate <Plymatea@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -4,28 +4,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import {newArray} from './js/project.js';
 
-function newArray(wantedLength = 50) {
+async function newArray(wantedLength = 50) {
   let array = [];
   for (let i = 0; i < wantedLength; i++) {
     let random = Math.floor(Math.random() * 110) + 10;
     array.push(random);
   }
 
-  const bars = document.querySelector('#bars');
-  bars.innerHTML = '';
+  const speed = 1000 - $('#delay').val();
+  const bars = $('#bars');
+  bars.html('');
 
   for (let i = 0; i < wantedLength; i++) {
     const bar = document.createElement('div');
     bar.style.height = `${array[i] * 2}px`;
     bar.classList.add('flex-item');
-    bars.appendChild(bar);
+    await waitforme(speed);
+    bars.append(bar);
   }
+}
+
+function waitforme(milisec) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('');
+    }, milisec);
+  });
 }
 
 $(document).ready(function () {
   $('#random-array').click(function () {
     let arrayWidth = $('#array-width').val();
-    console.log(arrayWidth);
     newArray(arrayWidth);
   });
 });
